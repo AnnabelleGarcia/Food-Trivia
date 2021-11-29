abc632a8f06f581ee2fd2b10f2ac2748e11813ca
AnnabelleGarcia <46617701+AnnabelleGarcia@users.noreply.github.com>
2021-11-29 02:01:16 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,140 @@
+// Questions will be asked
+const Questions = [{
+    id: 0,
+    q: "What is the rarest M&M color?",
+    a: [{ text: "Brown", isCorrect: true },
+        { text: "Red", isCorrect: false },
+        { text: "Yellow", isCorrect: false },
+        { text: "Green", isCorrect: false }
+    ]
+
+},
+{
+    id: 1,
+    q: "What is the most consumed manufactured drink in the world?",
+    a: [{ text: "Slushie", isCorrect: false, isSelected: false },
+        { text: "Soda", isCorrect: false },
+        { text: "Juice", isCorrect: false },
+        { text: "Tea", isCorrect: true }
+    ]
+
+},
+{
+    id: 2,
+    q: "Which country invented ice cream?",
+    a: [{ text: "Canada", isCorrect: false },
+        { text: "Italy", isCorrect: false },
+        { text: "China", isCorrect: true },
+        { text: "Germany", isCorrect: false }
+    ]
+
+}
+
+]
+
+// Set start
+var start = true;
+
+// Iterate
+function iterate(id) {
+
+// Getting the result display section
+var result = document.getElementsByClassName("result");
+result[0].innerText = "";
+
+// Getting the question
+const question = document.getElementById("question");
+
+
+// Setting the question text
+question.innerText = Questions[id].q;
+
+// Getting the options
+const op1 = document.getElementById('op1');
+const op2 = document.getElementById('op2');
+const op3 = document.getElementById('op3');
+const op4 = document.getElementById('op4');
+
+
+// Providing option text
+op1.innerText = Questions[id].a[0].text;
+op2.innerText = Questions[id].a[1].text;
+op3.innerText = Questions[id].a[2].text;
+op4.innerText = Questions[id].a[3].text;
+
+// Providing the true or false value to the options
+op1.value = Questions[id].a[0].isCorrect;
+op2.value = Questions[id].a[1].isCorrect;
+op3.value = Questions[id].a[2].isCorrect;
+op4.value = Questions[id].a[3].isCorrect;
+
+var selected = "";
+
+// Show selection for op1
+op1.addEventListener("click", () => {
+    op1.style.backgroundColor = "silver";
+    op2.style.backgroundColor = "";
+    op3.style.backgroundColor = "";
+    op4.style.backgroundColor = "";
+    selected = op1.value;
+})
+
+// Show selection for op2
+op2.addEventListener("click", () => {
+    op1.style.backgroundColor = "";
+    op2.style.backgroundColor = "silver";
+    op3.style.backgroundColor = "";
+    op4.style.backgroundColor = "";
+    selected = op2.value;
+})
+
+// Show selection for op3
+op3.addEventListener("click", () => {
+    op1.style.backgroundColor = "";
+    op2.style.backgroundColor = "";
+    op3.style.backgroundColor = "silver";
+    op4.style.backgroundColor = "";
+    selected = op3.value;
+})
+
+// Show selection for op4
+op4.addEventListener("click", () => {
+    op1.style.backgroundColor = "";
+    op2.style.backgroundColor = "";
+    op3.style.backgroundColor = "";
+    op4.style.backgroundColor = "silver";
+    selected = op4.value;
+})
+
+// Grabbing the evaluate button
+const evaluate = document.getElementsByClassName("evaluate");
+
+// Evaluate method
+evaluate[0].addEventListener("click", () => {
+    if (selected == "true") {
+        result[0].innerHTML = "True";
+        result[0].style.color = "green";
+    } else {
+        result[0].innerHTML = "False";
+        result[0].style.color = "red";
+    }
+})
+}
+
+if (start) {
+iterate("0");
+}
+
+// Next button and method
+const next = document.getElementsByClassName('next')[0];
+var id = 0;
+
+next.addEventListener("click", () => {
+start = false;
+if (id < 2) {
+    id++;
+    iterate(id);
+    console.log(id);
+}
+
+})
